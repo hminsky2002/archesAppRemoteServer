8b154fa7ad26e9bcd42ec79f5b42a2d35e5d0308
Harry Minsky <harry.minsky@gmail.com>
2025-07-08 23:21:53 -0400

    Add CORS support

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify
 import json
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app, supports_credentials=True)
+CORS(app, resources=r"/artist/*")
 
 with open("wac-artists.json", "r") as f:
     data = json.load(f)
